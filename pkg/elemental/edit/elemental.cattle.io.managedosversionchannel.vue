8150f1acd6228c5b5377e5aad9ebb858e75c9d3e
Alexandre Alves <aalves@Alexandres-MBP.lan>
2023-02-21 18:08:42 +0000

    working on create view for osversionchannels

---
+++ pkg/elemental/edit/elemental.cattle.io.managedosversionchannel.vue
@@ -0,0 +1,64 @@
+<script>
+import Loading from '@shell/components/Loading.vue';
+import CruResource from '@shell/components/CruResource.vue';
+import CreateEditView from '@shell/mixins/create-edit-view';
+import { LabeledInput } from '@components/Form/LabeledInput';
+import NameNsDescription from '@shell/components/form/NameNsDescription';
+
+export default {
+  name:       'ManagedOsVersionChannelEditView',
+  components: {
+    Loading, LabeledInput, CruResource, NameNsDescription
+  },
+  mixins:     [CreateEditView],
+  props:      {
+    value: {
+      type:     Object,
+      required: true
+    },
+    mode: {
+      type:     String,
+      required: true
+    },
+  },
+  async fetch() {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods:  {},
+};
+</script>
+
+<template>
+  <Loading v-if="!value" />
+  <CruResource
+    v-else
+    :done-route="doneRoute"
+    :can-yaml="true"
+    :mode="mode"
+    :resource="value"
+    :errors="errors"
+    @error="e=>errors = e"
+    @finish="save"
+    @cancel="done"
+  >
+    <div class="row mt-40 mb-20">
+      <div class="col span-12 mb-20">
+        <h3>{{ t('elemental.osversionchannels.create.configuration') }}</h3>
+        <NameNsDescription v-model="value" :mode="mode" :description-hidden="true" :namespaced="false" />
+      </div>
+    </div>
+    <div v-if="value.spec" class="row mb-20">
+      <div class="col span-6 mb-20">
+        <h3>{{ t('elemental.osversionchannels.create.spec') }}</h3>
+        <LabeledInput
+          v-model.trim="value.spec.options.image"
+          :label="t('elemental.osversionchannels.create.imageRegistry.label')"
+          :placeholder="t('elemental.osversionchannels.create.imageRegistry.placeholder', null, true)"
+          :mode="mode"
+        />
+      </div>
+    </div>
+  </CruResource>
+</template>
---
+++ pkg/elemental/models/elemental.cattle.io.managedosversionchannel.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import { _CREATE } from '@shell/config/query-params';
+import { ELEMENTAL_DEFAULT_NAMESPACE } from '../types';
+import ElementalResource from './elemental-resource';
+
+export default class ManagedOsVersionChannel extends ElementalResource {
+  applyDefaults(vm, mode) {
+    if ( !this.spec ) {
+      Vue.set(this, 'spec', { options: { image: '' } });
+    }
+    if ( !this.metadata || mode === _CREATE ) {
+      Vue.set(this, 'metadata', { namespace: ELEMENTAL_DEFAULT_NAMESPACE });
+    }
+  }
+}
