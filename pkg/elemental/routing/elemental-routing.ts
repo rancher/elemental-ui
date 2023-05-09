b891bf9334ac6b4feadad07b622382b4111417e9
Alexandre Alves <aalves@Alexandres-MBP.lan>
2023-05-09 11:22:12 +0100

    update routes definition to mitigate any route collision/undefinition problems

--- pkg/elemental/routing/elemental-routing.ts
+++ pkg/elemental/routing/elemental-routing.ts
@@ -4,31 +4,58 @@ import ListElementalResource from '../pages/_resource/index.vue';
 import CreateElementalResource from '../pages/_resource/create.vue';
 import ElementalResourceDetails from '../pages/_resource/_id.vue';
 
+const BLANK_CLUSTER = '_';
+
 const routes = [
   {
     name:      `${ ELEMENTAL_PRODUCT_NAME }-c-cluster`,
     path:      `/:product/c/:cluster/dashboard`,
     component: Dashboard,
+    meta:      {
+      product: ELEMENTAL_PRODUCT_NAME,
+      cluster: BLANK_CLUSTER,
+      pkg:     ELEMENTAL_PRODUCT_NAME
+    }
   },
   {
     name:      `${ ELEMENTAL_PRODUCT_NAME }-c-cluster-resource`,
     path:      `/:product/c/:cluster/:resource`,
     component: ListElementalResource,
+    meta:      {
+      product: ELEMENTAL_PRODUCT_NAME,
+      cluster: BLANK_CLUSTER,
+      pkg:     ELEMENTAL_PRODUCT_NAME
+    }
   },
   {
     name:      `${ ELEMENTAL_PRODUCT_NAME }-c-cluster-resource-create`,
     path:      `/:product/c/:cluster/:resource/create`,
     component: CreateElementalResource,
+    meta:      {
+      product: ELEMENTAL_PRODUCT_NAME,
+      cluster: BLANK_CLUSTER,
+      pkg:     ELEMENTAL_PRODUCT_NAME
+    }
   },
   {
     name:      `${ ELEMENTAL_PRODUCT_NAME }-c-cluster-resource-id`,
     path:      `/:product/c/:cluster/:resource/:id`,
     component: ElementalResourceDetails,
+    meta:      {
+      product: ELEMENTAL_PRODUCT_NAME,
+      cluster: BLANK_CLUSTER,
+      pkg:     ELEMENTAL_PRODUCT_NAME
+    }
   },
   {
     name:      `${ ELEMENTAL_PRODUCT_NAME }-c-cluster-resource-namespace-id`,
     path:      `/:product/c/:cluster/:resource/:namespace/:id`,
     component: ElementalResourceDetails,
+    meta:      {
+      product: ELEMENTAL_PRODUCT_NAME,
+      cluster: BLANK_CLUSTER,
+      pkg:     ELEMENTAL_PRODUCT_NAME
+    }
   },
 ];
 
--- pkg/elemental/utils/custom-routing.ts
+++ pkg/elemental/utils/custom-routing.ts
@@ -4,13 +4,22 @@ const BLANK_CLUSTER = '_';
 
 export const rootElementalRoute = () => ({
   name:    `${ ELEMENTAL_PRODUCT_NAME }-c-cluster`,
-  params: { product: ELEMENTAL_PRODUCT_NAME, cluster: BLANK_CLUSTER }
+  params: { product: ELEMENTAL_PRODUCT_NAME, cluster: BLANK_CLUSTER },
+  meta:   {
+    product: ELEMENTAL_PRODUCT_NAME,
+    cluster: BLANK_CLUSTER,
+    pkg:     ELEMENTAL_PRODUCT_NAME
+  },
 });
 
-export const createElementalRoute = (name: string, params: Object) => ({
+export const createElementalRoute = (name: string, params: Object, meta: Object) => ({
   name:   `${ rootElementalRoute().name }-${ name }`,
   params: {
     ...rootElementalRoute().params,
     ...params
+  },
+  meta: {
+    ...rootElementalRoute().meta,
+    ...meta
   }
 });
