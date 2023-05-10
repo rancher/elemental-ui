a0d177c5923cba33c009ddcaa8efe5193080fdcd
Alexandre Alves <aalves@Alexandres-MBP.lan>
2023-05-10 09:29:42 +0100

    nuxt removal

--- nuxt.config.js
+++
@@ -1,9 +0,0 @@
-import config, { API_PATH, proxyOpts } from '@rancher/shell/nuxt.config';
-
-export default config(__dirname, {
-  excludes:   [],
-  autoImport: [],
-  proxies: {
-    '/elemental/registration': proxyOpts(API_PATH)
-  }
-});
