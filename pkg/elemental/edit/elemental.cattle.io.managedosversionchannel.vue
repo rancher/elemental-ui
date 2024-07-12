62c87fb4e4bf5a0daf6f660f740619e0b4dbbc3e
Alexandre Alves <alexandre.alves@suse.com>
2024-07-12 12:22:21 +0100

    working on managed os channel automatic deletion

--- pkg/elemental/edit/elemental.cattle.io.managedosversionchannel.vue
+++ pkg/elemental/edit/elemental.cattle.io.managedosversionchannel.vue
@@ -4,11 +4,16 @@ import CruResource from '@shell/components/CruResource.vue';
 import CreateEditView from '@shell/mixins/create-edit-view';
 import { LabeledInput } from '@components/Form/LabeledInput';
 import NameNsDescription from '@shell/components/form/NameNsDescription';
+import Checkbox from '@components/Form/Checkbox/Checkbox.vue';
 
 export default {
   name:       'ManagedOsVersionChannelEditView',
   components: {
-    Loading, LabeledInput, CruResource, NameNsDescription
+    Loading,
+    LabeledInput,
+    CruResource,
+    NameNsDescription,
+    Checkbox
   },
   mixins:     [CreateEditView],
   props:      {
@@ -20,7 +25,14 @@ export default {
       type:     String,
       required: true
     }
-  }
+  },
+  computed: {
+    name() {
+      console.log('this.value', this.value);
+
+      return 'some-text';
+    }
+  },
 };
 </script>
 
@@ -44,14 +56,22 @@ export default {
       </div>
     </div>
     <div v-if="value.spec" class="row mb-20">
-      <div class="col span-6 mb-20">
+      <div class="col span-8 mb-20">
         <h3>{{ t('elemental.osversionchannels.create.spec') }}</h3>
+        {{ name }}
         <LabeledInput
           v-model.trim="value.spec.options.image"
           data-testid="os-version-channel-path"
           :label="t('elemental.osversionchannels.create.registryUri.label')"
           :placeholder="t('elemental.osversionchannels.create.registryUri.placeholder', null, true)"
           :mode="mode"
+        />
+        <Checkbox
+          v-model="value.spec.deleteNoLongerInSyncVersions"
+          :mode="mode"
+          :label="t('elemental.osversionchannels.create.automaticDelete')"
+          data-testid="os-version-channel-automatic-deletion"
+          class="mt-20"
         />
       </div>
     </div>
