dcdb58a337481bbc6ab8b9b79037f6d81cc4b6c8
Alexandre Alves <alexandre.alves@suse.com>
2024-08-06 14:29:11 +0100

    backport of PR 195

--- pkg/elemental/edit/elemental.cattle.io.managedosversionchannel.vue
+++ pkg/elemental/edit/elemental.cattle.io.managedosversionchannel.vue
@@ -4,11 +4,16 @@ import CruResource from '@shell/components/CruResource.vue';
 import CreateEditView from '@shell/mixins/create-edit-view';
 import { LabeledInput } from '@components/Form/LabeledInput';
 import NameNsDescription from '@shell/components/form/NameNsDescription';
+import Checkbox from '@components/Form/Checkbox/Checkbox.vue';
 
 export default {
   name:       'ManagedOsVersionChannelEditView',
   components: {
-    Loading, LabeledInput, CruResource, NameNsDescription
+    Loading,
+    LabeledInput,
+    CruResource,
+    NameNsDescription,
+    Checkbox
   },
   mixins:     [CreateEditView],
   props:      {
@@ -44,7 +49,7 @@ export default {
       </div>
     </div>
     <div v-if="value.spec" class="row mb-20">
-      <div class="col span-6 mb-20">
+      <div class="col span-8 mb-20">
         <h3>{{ t('elemental.osversionchannels.create.spec') }}</h3>
         <LabeledInput
           v-model.trim="value.spec.options.image"
@@ -52,6 +57,13 @@ export default {
           :label="t('elemental.osversionchannels.create.registryUri.label')"
           :placeholder="t('elemental.osversionchannels.create.registryUri.placeholder', null, true)"
           :mode="mode"
+        />
+        <Checkbox
+          v-model="value.spec.deleteNoLongerInSyncVersions"
+          :mode="mode"
+          :label="t('elemental.osversionchannels.create.automaticDelete')"
+          data-testid="os-version-channel-automatic-deletion"
+          class="mt-20"
         />
       </div>
     </div>
